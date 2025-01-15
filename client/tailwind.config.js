e6def286368d4f2563d55cd0c90b044b60ad3ce1
Pingorochoo <manuelalonsomartinezvargas@gmail.com>
2025-01-14 21:03:10 -0700

    Initialize project, setup structure and add Header component

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F5385D',
+        'secondary': '#ffffff',
+        'accent': '#000000',
+        'background': '#ffffff',
+        'text': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
+
