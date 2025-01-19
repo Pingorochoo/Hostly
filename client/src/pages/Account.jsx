fb9816918dd2297c5ab0668092c159f2c52098d0
Pingorochoo <manuelalonsomartinezvargas@gmail.com>
2025-01-18 19:50:23 -0700

    Create account page and account navbar complete (missing bookings and places rendering)

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import AccountNavbar from "../components/AccountNavbar";
+import { useUser } from "../store";
+
+const Account = () => {
+  const { user, setUser } = useUser();
+
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "account";
+  }
+  async function logout() {
+    await axios.post("/users/logout");
+    setUser(null);
+  }
+
+  return (
+    <div>
+      <AccountNavbar subpage={subpage} />
+      {subpage === "account" && (
+        <div className="text-center max-w-lg mx-auto w-full mt-8">
+          logged in as {user?.name} ({user?.email})<br />
+          <button className="primary max-w-sm mt-2" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Account;
