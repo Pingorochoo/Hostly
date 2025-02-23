c5f62aae2f56efc31dff14aa1e4f01e3ee800cf2
Pingorochoo <manuelalonsomartinezvargas@gmail.com>
2025-02-22 17:20:38 -0700

    feat: add routes for creating and fetching user places

--- api/src/controllers/place.js
+++ api/src/controllers/place.js
@@ -1,6 +1,8 @@
 const download = require("image-downloader");
 const { join, parse } = require("path");
 const { rename } = require("fs/promises");
+const { verifyToken } = require("../config/jwtToken");
+const Place = require("../models/Place");
 const uploadPhotoByUrl = async (req, res) => {
   try {
     const { imageUrl } = req.body;
@@ -21,11 +23,53 @@ const uploadPhotoByUrl = async (req, res) => {
   }
 };
 const uploadPhotos = async (req, res) => {
-    const files = req.files;
-    const filenamamesArray = files.map((file) => file.filename);
-    res.json(filenamamesArray);
+  const files = req.files;
+  const filenamamesArray = files.map((file) => file.filename);
+  res.json(filenamamesArray);
+};
+const createPlace = async (req, res) => {
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+  } = req.body;
+  const { token } = req.cookies;
+  const { id } = verifyToken(token);
+  if (id) {
+    const placeDoc = await Place.create({
+      owener: id,
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    });
+    res.json(placeDoc);
+  } else res.status(400).json("Invalid token");
+};
+const getUserPlaces = async (req, res) => {
+  const { token } = req.cookies;
+  const { id } = verifyToken(token);
+  if (id) {
+    const places = await Place.find({ owener: id });
+    res.json(places);
+  } else res.json("Invalid token");
 };
 module.exports = {
   uploadPhotoByUrl,
   uploadPhotos,
+  createPlace,
+  getUserPlaces,
 };
--- api/src/routes/place.js
+++ api/src/routes/place.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
-const { uploadPhotoByUrl, uploadPhotos } = require("../controllers/place");
+const { uploadPhotoByUrl, uploadPhotos, createPlace, getUserPlaces } = require("../controllers/place");
 const { uploadPhotosMiddleware } = require("../middlwares/upload");
 const placeRouter = Router();
 placeRouter.post("/photos/url", uploadPhotoByUrl);
 placeRouter.post("/photos/upload", uploadPhotosMiddleware,uploadPhotos);
+placeRouter.post("/", createPlace);
+
+placeRouter.get("/", getUserPlaces);
 module.exports = placeRouter;
